6ffbf501262c1d38358418575e43df84667be288
mariella1402 <mariiellachiicas14@gmail.com>
2023-02-03 18:49:51 -0600

    MEJORAMIENTO

--- app/src/main/java/com/example/myapplicationpractica03dsm104/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationpractica03dsm104/MainActivity.kt
@@ -1,13 +1,44 @@
 package com.example.myapplicationpractica03dsm104
 
+import android.content.ContentValues.TAG
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-
+import kotlin.math.log
+private const val TAG ="PRUEBA"
 class MainActivity : AppCompatActivity() {
+    private lateinit var txtNumber1: EditText
+    private lateinit var txtNumber2: EditText
+    private lateinit var btnSumar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        txtNumber1=findViewById<EditText>(R.id.IdNumber1)
+        txtNumber2=findViewById<EditText>(R.id.IdNumber2)
+        btnSumar=findViewById<Button>(R.id.idbutton)
+
+        btnSumar.setOnClickListener{
+            var producto : Int
+            var num1: Int
+            var num2 : Int
+
+
+            num1=Integer.parseInt(txtNumber1.text.toString().trim())
+            num2=Integer.parseInt(txtNumber2.text.toString().trim())
+            producto = num1+num2
+
+            Log.d(TAG, "la suma de ${num1}+${num2}= ${producto}")
+
+
+        }
+
+
         lifecycle.addObserver(MiLifeCycleObserver())
         Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show()
     }
